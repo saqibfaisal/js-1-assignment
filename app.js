3a9bb407e0a7b73c2c506f78a377452f3cd5de3f
fardeen15 <fardeenmemon17@gmail.com>
2022-04-07 04:41:44 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,178 @@
+// chapter 1
+// question 1
+// alert("HELLO WORLD");
+
+//question2
+// alert(" Error!Please enter a valid password");
+
+//question3
+// alert("welcome to JS Land \n Happy Coding");
+
+//question4
+// alert(" Welcome to JS Land")
+// alert("Happy Coding! \n Prevent this page from creating additional dialogs");
+
+//question5
+// console.log("Hello...I can run JS through my web browser console");
+
+
+
+
+
+
+//chapter2
+
+//question1
+// var username;
+
+//question2
+// var myName = "SAQIB FAISAL"
+
+//question3
+// var message 
+// message =  " HELLO WORLD "
+// alert("message")
+
+//question4
+// var name = "SAQIB FAISAL"
+// alert(" name ")
+// var age = "15 YEARS OLD"
+// alert(" age ")
+// var certificate = "Certified Mobile Application Development"
+// alert(" certicate")
+
+//question5
+// console.log("PIZZA\nPIZZ\nPIZ\nPI\nP")
+
+//question6
+// var email = "saqibfaisalofficial.com@gmail.com"
+// alert("My Email address is" +email)
+
+//question7
+// var book = "A Smarter way to learn JavaScript"
+// alert("I am trying to learn from the book"+book)
+
+//question8
+// var output="Yah!I can wrie HTML content through JavaScript"
+// document.write(output)
+
+//question9
+// var design="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert("design")
+
+//chapter3
+//question1
+// var age = 16
+// alert("I am"+age+"year old")
+
+//question2
+// var n =14
+// alert("I am visited this site"+n+"time")
+
+//question3
+// var birthyear = 2006
+// document.write("My bith year is "+birthyear+"<br>Data type my decalared variable is number ")
+
+//question4
+// var visitorname ="SAQIB"
+// var Product ="T-Shirt"
+// var quantity = 5
+// document.write(visitorname+"ordered"+quantity+Product+"on XYZ Clothing store")
+
+//chapter4
+//question1
+// var name,age,address
+
+//question2
+//legal variable names
+// var name 
+// var full_name
+// var $lastname
+// var name123
+// var na123me
+
+//illegal variable names 
+// var 123name 
+// var full+last
+// var full name
+// var last-name 
+// var name# 
+
+//question3
+
+
+//chapter5
+//question1
+// var num1 =3
+// var num2 =5
+// var num3 =num1 + num2
+// document.write("Sum of 3 and 5 is"+num3)
+
+//question2
+//subtraction
+// var num1 =3
+// var num2 =5
+// var num3 =num1 - num2
+// document.write("sum of 3 and 5 is "+num3)
+
+//multipilcation
+// var num1 =3
+// var num2 =5
+// var num3 =num1 * num2
+// document.write("sum of 3 and 5 is"+num3)
+
+//division
+// var num1 =3
+// var num2 =5
+// var num3 =num1 / num2
+// document.write("sum of 3 and 5 is "+num3)
+
+//modulus
+// var num1 =3
+// var num2 =5
+// var num3 =num1 % num2
+// document.write("sum of 3 and 5 is"+num3)
+
+
+//question3
+//declare
+// var num
+// document.write("Value after variable declaration is"+num)
+//intial
+// num=5
+// document.write("<br>Initial value:"+num)
+//increment
+// num++
+// document.write("<br>value after increment is :"+num)
+// num=num+7
+// document.write("<br>value after addition is : "+num)
+//decrement
+// num--
+// document.write("<br>value after decrement is "+num)
+// num=num%3
+// document.write("<br>The reminder is"+num) 
+
+//question4
+// var cost =600
+// var totalcost =600*5
+// document.write("Total cost to buy 5 ticket to a movie is"+totalcost+ "PKR")
+
+//question5
+// document.write("Table of 4")
+// document.write("<br>4x1="+4*1)
+// document.write("<br>4x2="+4*2)
+// document.write("<br>4x3="+4*3)
+// document.write("<br>4x4="+4*4)
+// document.write("<br>4x5="+4*5)
+// document.write("<br>4x6="+4*6)
+// document.write("<br>4x7="+4*7)
+// document.write("<br>4x8="+4*8)
+// document.write("<br>4x9="+4*9)
+// document.write("<br>4x10="+4*10)
+
+//question6
+
+
+//question7
+price1 = +prompt("Input price of item 1")
+console.log("price of item 1 is "+price1)
